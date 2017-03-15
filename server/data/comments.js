549b4ac10b2e3fbc09a9e9925fd17174f6aafa18
Joseph Kim <canyouspelljoe@gmail.com>
2017-03-14 19:49:20 -0700

    added mock data and completed schema, working on html template

--- server/data/comments.js
+++ server/data/comments.js
@@ -1,32 +1,64 @@
 const CommentList = [
   {
-    _id: 'cid-' + Math.ceil(Math.random() * 99999999),
-    content: 'This is a very good blog post',
-    author: 'pahan'
+    _id: 0,
+    content: 'I made those. Youre welcome',
+    user: 'yeezy'
   },
-
   {
-    _id: 'cid-' + Math.ceil(Math.random() * 99999999),
-    content: 'Keep up the good work',
-    author: 'indi'
-  }
+    _id: 1,
+    content: 'thx kanye!',
+    user: 'leonard'
+  },
+  {
+    _id: 2,
+    content: 'looking fly',
+    user: 'wusselrestbrook'
+  },
+  {
+    _id: 3,
+    content: 'nice ride but I got better <3',
+    user: 'kimk'
+  },
+  {
+    _id: 4,
+    content: 'what a beautiful picture!',
+    user: 'kimk'
+  },
+  {
+    _id: 5,
+    content: 'I know. I took it',
+    user: 'yeezy'
+  },
+  {
+    _id: 6,
+    content: 'yeah I bet you do after your last night lol',
+    user: 'wusselrestbrook'
+  },
+  {
+    _id: 7,
+    content: 'nice place homie',
+    user: 'yeezy'
+  },
+  {
+    _id: 8,
+    content: 'you are amazing honey',
+    user: 'kimk'
+  },
+  {
+    _id: 9,
+    content: 'yassss slay kanye',
+    user: 'leonard'
+  },
+  {
+    _id: 10,
+    content: 'ilovethis',
+    user: 'jerry'
+  },
+  {
+    _id: 11,
+    content: "y'all tripping",
+    user: 'wusselrestbrook'
+  },
 ];
 
-const ReplyList = [
-  {
-    _id: 'cid-' + Math.ceil(Math.random() * 99999999),
-    content: 'Thank You!',
-    author: 'arunoda'
-  },
-
-  {
-    _id: 'cid-' + Math.ceil(Math.random() * 99999999),
-    content: 'If you need more information, just contact me.',
-    author: 'arunoda'
-  },
-];
-
-export default {
-  CommentList,
-  ReplyList
-};
+export default CommentList;
